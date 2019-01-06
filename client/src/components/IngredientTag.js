1d839015005e4301fede290a07434733f9299d30
MaciejReimann <maciej.reimann@gmail.com>
2019-01-07 00:13:15 +0100

    is clickable

--- client/src/components/IngredientTag.js
+++ client/src/components/IngredientTag.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function IngredientTag({ content }) {
-  return <div>{`#${content}`}</div>;
+export default function IngredientTag({ content, symbol, onClick }) {
+  let displayedSymbol;
+  if (symbol === "+") {
+    displayedSymbol = <i className="tag-icon fas fa-plus" />;
+  }
+  return (
+    <button className="tag" onClick={() => onClick(content)}>
+      {`#${content} `}
+      {displayedSymbol}
+    </button>
+  );
 }
